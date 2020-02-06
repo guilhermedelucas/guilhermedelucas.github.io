1af7e366ea3bc08f56acec39b07fa950ec57c2fd
guilhermedelucas <gdelucasde@gmail.com>
2020-02-06 18:19:28 +0100

    Updates

---
+++ precache-manifest.4e1ebf883fbef7eb4cbb8c3ff63206f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7535eaa5b4c097c53ca563f35267822",
+    "url": "/guilhermedelucas/guilhermedelucas.github.io/index.html"
+  },
+  {
+    "revision": "687cabf71a7445939670",
+    "url": "/guilhermedelucas/guilhermedelucas.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6002b7a30bb8a94a4fa9",
+    "url": "/guilhermedelucas/guilhermedelucas.github.io/static/js/2.cd02380a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/guilhermedelucas/guilhermedelucas.github.io/static/js/2.cd02380a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687cabf71a7445939670",
+    "url": "/guilhermedelucas/guilhermedelucas.github.io/static/js/main.af293c0c.chunk.js"
+  },
+  {
+    "revision": "e2e8ed8e7cb807d040d6",
+    "url": "/guilhermedelucas/guilhermedelucas.github.io/static/js/runtime-main.b30b16c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/guilhermedelucas/guilhermedelucas.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
